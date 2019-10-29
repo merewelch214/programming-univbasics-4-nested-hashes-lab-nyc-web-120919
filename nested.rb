09bce7372adfe9968b979b5626c567d0a1c70553
comical-sandbox-7108 <merewelch@gmail.com>
2019-10-29 02:29:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
         }
      }
     
-  programmer_hash[:yukihiro_matsumoto] => {
-  :known_for => "Ruby",
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
     :languages => ["LISP", "C"]
 }
 
