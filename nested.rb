0e0727fee4a15b0f8b0b836f3bc8c248030e8678
comical-sandbox-7108 <merewelch@gmail.com>
2019-10-29 02:37:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
         }
      }
      
-    programmer_hash[:dennis_ritchie][:languages].unshift("Assembly")
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
     return programmer_hash
 end
